94dbdfca468c27933d9a26808edd488748d28a84
Ritika <ritssharma0812@gmail.com>
2024-08-11 00:05:23 +0530

    This is my second commit

---
+++ app/src/main/java/DictionaryApi.kt
@@ -0,0 +1,11 @@
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Path
+
+interface DictionaryApi {
+
+    @GET("en/{word}")
+    suspend fun getMeaning(@Path("word") word: String): Response<List<WordResult>>
+}
+
+
